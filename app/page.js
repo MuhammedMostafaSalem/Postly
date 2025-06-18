7d681cc3f1877409fdb8d329b0ae5514f68bf04c
mohammed <marwansalem164@gmail.com>
2025-06-18 20:43:13 +0300

    Install app

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-teal-200">Hello, Next.js!</h1>
+    </div>
+  );
+}
