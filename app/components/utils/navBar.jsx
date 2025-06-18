0e42a49b0be32ebde0ef4e05a8c077e77ba389a9
mohammed <marwansalem164@gmail.com>
2025-06-18 22:00:29 +0300

    create navBar componet

---
+++ app/components/utils/navBar.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link"
+
+const NavBar = () => {
+    return (
+        <div className="w-full
+            flex
+            items-center
+            justify-between
+            py-4
+            mx-auto
+            container
+            transition-all
+            duration-1000"
+        >
+            <Link href="/" className="flex items-center gap-1">
+                <h2 className="font-normal text-2xl sm-max:text-lg text-black">
+                Postly
+                </h2>
+            </Link>
+
+            <div className="flex items-center gap-2">
+                <button className="bg-blue-600
+                text-white
+                px-4
+                py-2
+                rounded-md
+                hover:bg-blue-700
+                transition-all"
+            >
+                Create New Post
+                </button>
+            </div>
+        </div>
+
+    )
+}
+
+export default NavBar
