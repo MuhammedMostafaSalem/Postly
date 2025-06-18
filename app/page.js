2fa47e587ef4d27dc60630fe56660e7b72ad5fd1
mohammed <marwansalem164@gmail.com>
2025-06-18 23:42:49 +0300

    Add card component in home page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import CardComponent from "./components/utils/cardComponent";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-teal-200">Hello, Next.js!</h1>
+      <h3 className="text-xl font-semibold py-5 text-center">Blog Post</h3>
+
+      <div className="flex
+        justify-center
+        gap-4
+        items-start
+        flex-wrap
+        overflow-y-auto"
+      >
+        <CardComponent cardWidth='w-[400px]' typePage='home' />
+      </div>
     </div>
   );
 }
