b7cf144cfa72318a935abf15315eeb3ad06b4ba8
mohammed <marwansalem164@gmail.com>
2025-06-19 02:41:27 +0300

    Add ToastContainer

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
 import "@/styles/globals.css";
 import PagesLayout from "./PagesLayout";
+import { ToastContainer } from "react-toastify";
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <PagesLayout>
             <Component {...pageProps} />
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </PagesLayout>
     );
 }
