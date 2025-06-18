5314332eb1649b6ecc5f50ffd07824f534f638c5
mohammed <marwansalem164@gmail.com>
2025-06-19 00:17:51 +0300

    Update color btn

--- app/components/utils/navBar.jsx
+++ app/components/utils/navBar.jsx
@@ -19,16 +19,16 @@ const NavBar = () => {
             </Link>
 
             <div className="flex items-center gap-2">
-                <button className="bg-blue-600
+                <Link href='/createPost' className="bg-gray-600
                 text-white
                 px-4
                 py-2
                 rounded-md
-                hover:bg-blue-700
+                hover:bg-gray-700
                 transition-all"
             >
                 Create New Post
-                </button>
+                </Link>
             </div>
         </div>
 
